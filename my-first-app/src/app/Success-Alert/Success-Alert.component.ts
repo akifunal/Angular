622e596ccd38019a34feb0b2eb2186e56bd26c8f
Akif ÜNAL <akifunal05@gmail.com>
2019-09-10 15:30:32 +0300

    angular course continuous

---
+++ my-first-app/src/app/Success-Alert/Success-Alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-success-alert',
+    templateUrl: './Success-Alert.component.html',
+    styleUrls: ['./Success-Alert.component.css']
+})
+
+export class SuccessAlertComponent {
+
+}
