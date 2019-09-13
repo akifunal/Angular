97c1fccadad91a61ff53c8071cee896df9f095e7
Akif ÜNAL <akifunal05@gmail.com>
2019-09-13 11:40:39 +0300

    Start and pause implementation

--- cmp-databinding-assignment-start/src/app/app.component.ts
+++ cmp-databinding-assignment-start/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cmp-databinding-assignment-start';
+  onIntervalFired(firedNumber: number) {
+    console.log(firedNumber);
+  }
 }
