d3597c5f1d6f342bad05cca7206391bd9dbd4afc
Akif ÜNAL <akifunal05@gmail.com>
2019-09-19 17:16:28 +0300

    services section finished

--- ng-complete-course/src/app/app.module.ts
+++ ng-complete-course/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -29,7 +30,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ng-complete-course/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng-complete-course/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from "./../recipe.service";
 import { Recipe } from './../recipe.model';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
@@ -7,19 +8,12 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
