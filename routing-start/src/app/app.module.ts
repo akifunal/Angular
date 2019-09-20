b5fe38e01860d0720870736a1a2ce45687858515
Akif ÜNAL <akifunal05@gmail.com>
2019-09-20 17:00:58 +0300

    routing final

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -15,7 +15,9 @@ import { Routes, RouterModule } from '@angular/router';
 const appRoutes: Routes =[
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent },
+  { path: 'servers/:id/edit', component: EditServerComponent },
 ];
 
 @NgModule({
